c504b719aec9d315bd959e171b5cc5d9b88e20b1
Crowdbotics <engineering@crowdbotics.com>
2023-03-22 01:39:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/muddy_flower_dev_71170/MainActivity.java
@@ -0,0 +1,15 @@
+package com.muddy_flower_dev_71170;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "muddy_flower_dev_71170";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-flower-dev-71170",
+  url: "https://muddy-flower-dev-71170.botics.co",
+  api: "https://muddy-flower-dev-71170.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
